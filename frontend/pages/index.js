6672ccaa9d299ccbb307eb1367a8cd4d71524941
Maxx Borer <maxxborer@gmail.com>
2021-05-16 23:14:28 +0400

    fix: ci

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          ***REMOVED***
+          {`<${***REMOVED***}>`}
           <br />
           <br />
           vps + domain + docker + ci/cd
