0338a7c0172e5479f99795ca28e2bb9fde5dc296
Maxx Borer <maxxborer@gmail.com>
2021-05-16 23:17:33 +0400

    fix: ci

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          {`<${***REMOVED***}>`}
+          ***REMOVED***
           <br />
           <br />
           vps + domain + docker + ci/cd
@@ -20,8 +20,8 @@ export default function Home() {
         </h1>
 
         <p className={styles.title}>
-          <a className={styles.code} href="https://gitlab.com/***REMOVED***/next-starter">
-            https://gitlab.com/***REMOVED***/next-starter
+          <a className={styles.code} href="https://gitlab.com/***REMOVED***/vps-cicd-starter">
+            gitlab.com/***REMOVED***/vps-cicd-starter
           </a>
         </p>
         </main>
